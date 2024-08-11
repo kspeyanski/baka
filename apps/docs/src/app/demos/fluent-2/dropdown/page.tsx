bdbcb5218b65525320c7ede775ddc3f7e7403162
Kiril Peyanski <peyanski@progress.com>
2024-08-11 15:25:52 +0300

    WIP(fluent): add dropdown component

--- apps/docs/src/app/demos/fluent-2/dropdown/page.tsx
+++ apps/docs/src/app/demos/fluent-2/dropdown/page.tsx
@@ -1,15 +1,44 @@
-import { BakaDialog, BakaIcon, BakaList, BakaListItem, BakaMenu, BakaText, BakaTextField } from "baka-ui";
+import { BakaDialog, BakaIcon, BakaList, BakaListItem, BakaMenu, BakaMenuItem, BakaText, BakaTextField } from "baka-ui";
 
 export default function DropdownDemo() {
   return (
-    <>
-      <BakaTextField style={{ width: 300 }} as="button">
-        <BakaText variant={"placeholder"}>Placeholder text</BakaText>
-        <BakaIcon></BakaIcon>
-      </BakaTextField>
-      <BakaMenu>
-        <BakaMenu>test</BakaMenu>
-      </BakaMenu>
-    </>
+    <div className="flex gap-4 flex-row">
+      {["outline", "transparent", "filled-lighter", "filled-darker"].map((variant) => (
+        <div key={variant} className="flex gap-5 flex-col">
+          <BakaTextField style={{ width: 300 }} as="button" variant={variant}>
+            <BakaText variant={"placeholder"}>Placeholder text</BakaText>
+            <BakaIcon></BakaIcon>
+          </BakaTextField>
+
+          <div>
+            <BakaTextField style={{ width: 300 }} as="button" variant={variant}>
+              <BakaText variant={"placeholder"}>Placeholder text</BakaText>
+              <BakaIcon></BakaIcon>
+            </BakaTextField>
+            <BakaMenu>
+              <BakaMenuItem readOnly>
+                <BakaText variant={"caption-1-strong"}>Action</BakaText>
+              </BakaMenuItem>
+              <BakaMenuItem>
+                <BakaIcon />
+                <BakaText>Action</BakaText>
+              </BakaMenuItem>
+              <BakaMenuItem>
+                <BakaIcon />
+                <BakaText>Action</BakaText>
+              </BakaMenuItem>
+              <BakaMenuItem>
+                <BakaIcon />
+                <BakaText>Action</BakaText>
+              </BakaMenuItem>
+              <BakaMenuItem>
+                <BakaIcon />
+                <BakaText>Action</BakaText>
+              </BakaMenuItem>
+            </BakaMenu>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
